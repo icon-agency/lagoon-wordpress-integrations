14c6e2ae2cb5f009d3b0e599e552fc145aad6d2c
Al Munnings <al.munnings@gmail.com>
2022-04-30 12:43:36 +1000

    Tabs sand spaces

--- src/LagoonLogs/LagoonHandler.php
+++ src/LagoonLogs/LagoonHandler.php
@@ -13,26 +13,26 @@ class LagoonHandler {
 
 	const LAGOON_LOGS_DEFAULT_CHUNK_SIZE_BYTES = 15000;
 
-  /**
-   * Lagoon logs hostname
-   *
-   * @var string
-   */
+	/**
+	 * Lagoon logs hostname
+	 *
+	 * @var string
+	 */
 	protected $host;
 
-  /**
-   * Lagoon logs port
-   *
-   * @var int
-   */
+	/**
+	 * Lagoon logs port
+	 *
+	 * @var int
+	 */
 	protected $port;
 
-  /**
-   * Helper class to get a handler and processor.
-   *
-   * @param string $host Lagoon logs host.
-   * @param int $port Lagoon logs port.
-   */
+	/**
+	 * Helper class to get a handler and processor.
+	 *
+	 * @param string $host Lagoon logs host.
+	 * @param int $port Lagoon logs port.
+	 */
 	public function __construct( $host, $port ) {
 		$this->host = $host;
 		$this->port = $port;
@@ -42,8 +42,8 @@ class LagoonHandler {
 	 * Monolog UDP socket handler.
 	 */
 	public function handler(): SocketHandler {
-    $connection = sprintf( 'udp://%s:%s', $this->host, $this->port );
-    
+		$connection = sprintf( 'udp://%s:%s', $this->host, $this->port );
+
 		$handler = new SocketHandler( $connection );
 		$handler->setChunkSize( self::LAGOON_LOGS_DEFAULT_CHUNK_SIZE_BYTES );
 
